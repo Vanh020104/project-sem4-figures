b49dc565d764e1719a37c25f21f8058bca714f29
PhuTam54 <phutamytb@gmail.com>
2023-09-02 03:00:55 +0700

    Update AccountItem.js

--- src/components/AccountItem/AccountItem.js
+++ src/components/AccountItem/AccountItem.js
@@ -1,10 +1,11 @@
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
-import styles from './AccountItem.module.scss';
-import Image from '~/components/Image';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
+import Image from '~/components/Image';
+import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
 
